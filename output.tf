fee400362db42a4dd1ab1000c7c0fcef1d017139
amit <amit@ow.com>
2024-11-22 06:19:04 +0000

    adding  output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "my_vpc" {
+/* output "my_vpc" {
   value = google_compute_network.main-vpc.id
 }
 output "my_subnet" {
@@ -6,4 +6,12 @@ output "my_subnet" {
 }
 output "gcp_nat_gateway" {
   value = google_compute_router_nat.main-nat.id
+} */
+output "network_name" {
+  value = google_compute_network.main-vpc.name
 }
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.subnet-1.name
+}
+
