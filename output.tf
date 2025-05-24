8400a34aade68f641489366944e4a76850235955
amit <amit@openwriteup.com>
2025-05-24 19:26:25 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,3 @@
-/* output "my_vpc" {
-  value = google_compute_network.main-vpc.id
-}
-output "my_subnet" {
-  value = google_compute_subnetwork.subnet-1.id
-}
-output "gcp_nat_gateway" {
-  value = google_compute_router_nat.main-nat.id
-} */
 output "network_name" {
   value = google_compute_network.main-vpc.name
 }
